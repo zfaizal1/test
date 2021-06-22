261a56f693e31360c6d28e6d7cbba3f9dde550ab
zfaizal1 <86250787+zfaizal1@users.noreply.github.com>
2021-06-22 12:47:25 +0800

    app.py
    test

---
+++ app.py
@@ -0,0 +1,41 @@
+import os
+from flask import Flask
+from flask import render_template
+import socket
+import random
+import os
+
+app = Flask(__name__)
+
+color_codes = {
+    "red": "#e74c3c",
+    "green": "#16a085",
+    "blue": "#2980b9",
+    "pink": "#be2edd",
+    "yellow": "#ffff00",
+    "white": "#ffffff",
+    "purple": "#7d3c98"
+}
+
+#color = os.environ.get('APP_COLOR') or random.choice(["red","green"])
+#color = os.environ.get('APP_COLOR') or random.choice(["white"])
+color = os.environ.get('APP_COLOR') or random.choice(["pink","blue","yellow"])
+
+@app.route("/")
+def main():
+    #return 'Hello'
+    print(color)
+    return render_template('hello.html', name=socket.gethostname(), color=color_codes[color])
+
+@app.route('/color/<new_color>')
+def new_color(new_color):
+    return render_template('hello.html', name=socket.gethostname(), color=color_codes[new_color])
+
+@app.route('/read_file')
+def read_file():
+    f = open("/data/testfile.txt")
+    contents = f.read()
+    return render_template('hello.html', name=socket.gethostname(), contents=contents, color=color_codes[color])
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="8080")
